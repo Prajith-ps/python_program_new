29584cc7c672245b2443cbccf6a907d1791379b8
Prajith-ps <138312306+Prajith-ps@users.noreply.github.com>
2024-09-03 10:29:45 +0530

    Add files via upload

---
+++ 29_exceptionn.py
@@ -0,0 +1,33 @@
+#try block,#except
+
+# try:
+#     numerator=10
+#     denominator=0
+#     result=numerator/denominator
+#     print(result)
+# except:
+#     print("error:denominator cannot be zero.")
+
+
+#else
+
+# try:
+#     num=int(input("enter the number:"))
+#     assert num %1==0
+# except:
+#     print("not an even number!")
+# else:
+#     reciprocal =1/num
+#     print(reciprocal)
+
+# finally
+
+# try:
+#     numerator=10
+#     denominator=0
+#     result=numerator/denominator
+#     print(result)
+# except:
+#     print("error:denominator cannot be zero.")
+# finally:
+#     print("this is finally bolck")
